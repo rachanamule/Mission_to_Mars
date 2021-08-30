0c92e0f63849e974932fbc2ef997b89fc9ccf6ce
rachanadharangutti <dharangutti.rachana@gmail.com>
2021-08-29 22:19:19 -0500

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -20,6 +20,7 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
+        "hemispheres": hemisphere_image_urls,
         "last_modified": dt.datetime.now()
     }
 
